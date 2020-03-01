8f5ad75658a78d421288b534a90eeff85de9d28f
Siwei Feng <ustcsiwei88@gmail.com>
2020-03-01 17:42:24 -0500

    physical expr

---
+++ expr/plot.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+f = open('data.txt', 'r')
+xs, ys, zs = [], [], []
+for l in f.readlines():
+	l = l.split(' ')
+	xs.append(float(l[0]))
+	ys.append(float(l[1]))
+	zs.append(float(l[2]))
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+def set_axes_radius(ax, origin, radius):
+    ax.set_xlim3d([origin[0] - radius, origin[0] + radius])
+    ax.set_ylim3d([origin[1] - radius, origin[1] + radius])
+    ax.set_zlim3d([origin[2] - radius, origin[2] + radius])
+
+def set_axes_equal(ax):
+    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc..  This is one possible solution to Matplotlib's
+    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+
+    Input
+      ax: a matplotlib axis, e.g., as output from plt.gca().
+    '''
+
+    limits = np.array([
+        ax.get_xlim3d(),
+        ax.get_ylim3d(),
+        ax.get_zlim3d(),
+    ])
+
+    origin = np.mean(limits, axis=1)
+    radius = 0.5 * np.max(np.abs(limits[:, 1] - limits[:, 0]))
+    set_axes_radius(ax, origin, radius)
+
+ax.plot(xs = xs, ys =ys, zs = zs )
+set_axes_equal(ax)
+plt.show()
